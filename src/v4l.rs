4367c28363e4ece2abd3b320309f63baa54386f9
Nathan Merritt <nathan@gnmerritt.net>
2020-11-06 15:09:28 -0700

    use image crate to save captures to PNG

--- src/v4l.rs
+++ src/v4l.rs
@@ -1,9 +1,8 @@
 #[cfg(feature = "v4l-libs")]
-fn frame_writer(name: &str, num_frames: i32) -> Result<(), Box<dyn std::error::Error>> {
-    use std::fs::File;
-    use std::io::prelude::*;
+pub fn frame_writer(name: &str, num_frames: i32) -> Result<(), Box<dyn std::error::Error>> {
     use v4l::prelude::*;
     use v4l::FourCC;
+    use image::{load_from_memory_with_format, ImageFormat};
 
     let mut dev = CaptureDevice::new(0)?;
 
@@ -11,7 +10,7 @@ fn frame_writer(name: &str, num_frames: i32) -> Result<(), Box<dyn std::error::E
     let mut fmt = dev.format()?;
     fmt.width = 1920;
     fmt.height = 1080;
-    fmt.fourcc = FourCC::new(b"YUYV");
+    fmt.fourcc = FourCC::new(b"MJPG"); // my camera also supports "YUYV"
     dev.set_format(&fmt)?;
 
     // The actual format chosen by the device driver may differ from what we
@@ -43,21 +42,22 @@ fn frame_writer(name: &str, num_frames: i32) -> Result<(), Box<dyn std::error::E
     for i in 0..num_frames {
         let frame = stream.next().unwrap();
         let filename = format!(
-            "{n}.{i}.{w}-{h}.yuyv",
+            "{n}.{i}.{w}-{h}",
             n = name,
             i = i,
             w = fmt.width,
             h = fmt.height
         );
+        let png = format!("{}.png", filename);
         println!(
             "Buffer size: {}, seq: {}, timestamp: {}  --> {}",
             frame.len(),
             frame.meta().sequence,
             frame.meta().timestamp,
-            filename,
+	    png,
         );
-        let mut file: File = File::create(filename)?;
-        file.write_all(frame.data())?;
+        let img = load_from_memory_with_format(frame.data(), ImageFormat::Jpeg)?;
+        img.save_with_format(png, ImageFormat::Png)?;
 
         // To process the captured data, you can pass it somewhere else.
         // If you want to modify the data or extend its lifetime, you have to
